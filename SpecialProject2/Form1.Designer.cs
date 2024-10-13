629d837864caf00a56631531c7f582e5bd1c2341
Regnazo <regnazo04@gmail.com>
2024-10-13 02:03:55 -0400

    Add files via upload

--- SpecialProject2/Form1.Designer.cs
+++ SpecialProject2/Form1.Designer.cs
@@ -28,23 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.DrawingPanel = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.XCord = new System.Windows.Forms.Label();
+            this.YCord = new System.Windows.Forms.Label();
+            this.ShapeWidth = new System.Windows.Forms.Label();
+            this.ShapeHeight = new System.Windows.Forms.Label();
+            this.XText = new System.Windows.Forms.TextBox();
+            this.YText = new System.Windows.Forms.TextBox();
+            this.WidthText = new System.Windows.Forms.TextBox();
+            this.HeightText = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // DrawingPanel
+            // 
+            this.DrawingPanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.DrawingPanel.Location = new System.Drawing.Point(128, 0);
+            this.DrawingPanel.Name = "DrawingPanel";
+            this.DrawingPanel.Size = new System.Drawing.Size(660, 438);
+            this.DrawingPanel.TabIndex = 4;
+            this.DrawingPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawingPanel_Paint);
+            this.DrawingPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawingPanel_MouseDown);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(1, 67);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(121, 44);
+            this.ColorButton.TabIndex = 6;
+            this.ColorButton.Text = "Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // XCord
+            // 
+            this.XCord.AutoSize = true;
+            this.XCord.Location = new System.Drawing.Point(19, 121);
+            this.XCord.Name = "XCord";
+            this.XCord.Size = new System.Drawing.Size(15, 16);
+            this.XCord.TabIndex = 7;
+            this.XCord.Text = "X";
+            // 
+            // YCord
+            // 
+            this.YCord.AutoSize = true;
+            this.YCord.Location = new System.Drawing.Point(69, 121);
+            this.YCord.Name = "YCord";
+            this.YCord.Size = new System.Drawing.Size(16, 16);
+            this.YCord.TabIndex = 8;
+            this.YCord.Text = "Y";
+            // 
+            // ShapeWidth
+            // 
+            this.ShapeWidth.AutoSize = true;
+            this.ShapeWidth.Location = new System.Drawing.Point(10, 165);
+            this.ShapeWidth.Name = "ShapeWidth";
+            this.ShapeWidth.Size = new System.Drawing.Size(41, 16);
+            this.ShapeWidth.TabIndex = 9;
+            this.ShapeWidth.Text = "Width";
+            // 
+            // ShapeHeight
+            // 
+            this.ShapeHeight.AutoSize = true;
+            this.ShapeHeight.Location = new System.Drawing.Point(66, 165);
+            this.ShapeHeight.Name = "ShapeHeight";
+            this.ShapeHeight.Size = new System.Drawing.Size(46, 16);
+            this.ShapeHeight.TabIndex = 10;
+            this.ShapeHeight.Text = "Height";
+            // 
+            // XText
+            // 
+            this.XText.Location = new System.Drawing.Point(2, 140);
+            this.XText.Name = "XText";
+            this.XText.Size = new System.Drawing.Size(52, 22);
+            this.XText.TabIndex = 12;
+            // 
+            // YText
+            // 
+            this.YText.Location = new System.Drawing.Point(60, 140);
+            this.YText.Name = "YText";
+            this.YText.Size = new System.Drawing.Size(52, 22);
+            this.YText.TabIndex = 13;
+            // 
+            // WidthText
+            // 
+            this.WidthText.Location = new System.Drawing.Point(2, 184);
+            this.WidthText.Name = "WidthText";
+            this.WidthText.Size = new System.Drawing.Size(52, 22);
+            this.WidthText.TabIndex = 14;
+            // 
+            // HeightText
+            // 
+            this.HeightText.Location = new System.Drawing.Point(57, 184);
+            this.HeightText.Name = "HeightText";
+            this.HeightText.Size = new System.Drawing.Size(55, 22);
+            this.HeightText.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HeightText);
+            this.Controls.Add(this.WidthText);
+            this.Controls.Add(this.YText);
+            this.Controls.Add(this.XText);
+            this.Controls.Add(this.ShapeHeight);
+            this.Controls.Add(this.ShapeWidth);
+            this.Controls.Add(this.YCord);
+            this.Controls.Add(this.XCord);
+            this.Controls.Add(this.ColorButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.DrawingPanel);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Paint += Form1_Paint;
-            this.MouseDown += Form1_MouseDown;
-            this.MouseUp += Form1_MouseUp;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Panel DrawingPanel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Label XCord;
+        private System.Windows.Forms.Label YCord;
+        private System.Windows.Forms.Label ShapeWidth;
+        private System.Windows.Forms.Label ShapeHeight;
+        private System.Windows.Forms.TextBox XText;
+        private System.Windows.Forms.TextBox YText;
+        private System.Windows.Forms.TextBox WidthText;
+        private System.Windows.Forms.TextBox HeightText;
     }
 }
 
--- SpecialProject2/Form1.cs
+++ SpecialProject2/Form1.cs
@@ -7,41 +7,134 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace SpecialProject2
 {
     public partial class Form1 : Form
     {
-        int x1, x2, x3, y2, y1;
+        //Overall integers for the class
+        private List<Shape> shapes = new List<Shape>();
+        private Shape currentShape;
+        private Color currentColor = Color.Black;
+        private string currentShapeType;
         public Form1()
         {
             InitializeComponent();
+            InitializeComboBox();
         }
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        // Adds the shape options to the combobox
+        private void InitializeComboBox()
         {
-            Graphics g = e.Graphics;
+            comboBox1.Items.Add("Rectangle");
+            comboBox1.Items.Add("Circle");
+        }
+        // Allows the panel to draw the shapes
+        private void DrawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw(e.Graphics);
+            }
+        }
+        //Sets the values in drawing panel to the shapes
+        private void DrawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            int x, y, width, height;
+            if (int.TryParse(XText.Text, out x) && int.TryParse(YText.Text, out y) &&
+                int.TryParse(WidthText.Text, out width) && int.TryParse(HeightText.Text, out height))
+            {
+                if (currentShapeType == "Rectangle")
+                    currentShape = new RectangleShape(x, y, width, height, currentColor);
+                else if (currentShapeType == "Circle")
+                    currentShape = new CircleShape(x, y, width, currentColor);
 
-            Pen pen = new Pen(Color.Red);
-            pen.Width = 6;
-            g.DrawRectangle(pen, 600, 50, 100, 100);
-            g.DrawEllipse(pen, 525, 150, 250, 250);
-            g.DrawEllipse(pen, 450, 400, 400, 400);
-            g.DrawEllipse(pen, 400, 800, 500, 500);
-            g.DrawLine(pen, 400, 150, 800, 100);
+                shapes.Add(currentShape);
+                DrawingPanel.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numeric values.");
+            }
         }
 
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            x1 = e.X;
-            y1 = e.Y;
-        }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        }
+        public void Form1_Load(object sender, EventArgs e)
         {
-            x2 = e.X;
-            y2 = e.Y;
-            Invalidate();
+
+        }
+        //Adds a color choosing menu which is very easy to use
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentColor = colorDialog.Color;
+                }
+            }
         }
     }
+    // Shape Class to define the shapes
+    public abstract class Shape
+        {
+            public Color Colortype { get; set; }
+
+            public Shape(Color color)
+            {
+                Colortype = color;
+            }
+            public abstract void Draw(Graphics g);
+        }
+        // Rectangle Shape
+        public class RectangleShape : Shape
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public RectangleShape(int x, int y, int width, int height, Color color) : base(color)
+            {
+                X = x;
+                Y = y;
+                Width = width;
+                Height = height;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                using (Brush brush = new SolidBrush(Colortype))
+                {
+                    g.FillRectangle(brush, X, Y, Width, Height);
+                }
+            }
+        }
+
+        // Circle Shape
+        public class CircleShape : Shape
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Radius { get; set; }
+
+            public CircleShape(int x, int y, int radius, Color color) : base(color)
+            {
+                X = x;
+                Y = y;
+                Radius = radius;
+            }
+
+            public override void Draw(Graphics g)
+            {
+                using (Brush brush = new SolidBrush(Colortype))
+                {
+                    g.FillEllipse(brush, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+                }
+            }
+        }
 }
 
